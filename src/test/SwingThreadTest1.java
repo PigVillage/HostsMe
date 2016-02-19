a85657f80b528414a2311046957c549976e2cbb4
TerrySolar <531012201@qq.com>
2016-02-19 17:48:03 +0800

    change class name

---
+++ src/test/SwingThreadTest1.java
@@ -0,0 +1,62 @@
+package test;
+
+import java.awt.FlowLayout;  
+import java.awt.event.ActionEvent;  
+import java.awt.event.ActionListener;  
+import javax.swing.JButton;  
+import javax.swing.JFrame;  
+import javax.swing.JProgressBar;  
+import javax.swing.JTextField;  
+public class SwingThreadTest1 extends JFrame {  
+    private static final long serialVersionUID = 1L;  
+    private static final String STR = "Completed : ";  
+    private JProgressBar progressBar = new JProgressBar();  
+    private JTextField text = new JTextField(10);  
+    private JButton start = new JButton("Start");  
+    private JButton end = new JButton("End");  
+    private boolean flag = false;  
+    private int count = 0;  
+    public SwingThreadTest1() {  
+        this.setLayout(new FlowLayout());  
+        add(progressBar);  
+        text.setEditable(false);  
+        add(text);  
+        add(start);  
+        add(end);  
+        start.addActionListener(new Start());  
+        end.addActionListener(new End());  
+    }  
+          
+    private void go() {  
+        while (count < 100) {  
+            try {  
+                Thread.sleep(100);//这里比作要完成的某个耗时的工作  
+            } catch (InterruptedException e) {  
+                e.printStackTrace();  
+            }  
+                         //更新进度条和输入框  
+            if (flag) {  
+                count++;  
+                progressBar.setValue(count);  
+                text.setText(STR + String.valueOf(count) + "%");  
+            }  
+        }  
+    }  
+    private class Start implements ActionListener {  
+        public void actionPerformed(ActionEvent e) {  
+            flag = true;//设置开始更新的标志  
+            go();//开始工作  
+        }  
+    }  
+    private class End implements ActionListener {  
+        public void actionPerformed(ActionEvent e) {  
+            flag = false;//停止  
+        }  
+    }  
+//    public static void main(String[] args) {  
+//        Test fg = new Test();  
+//        fg.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
+//        fg.setSize(300, 100);  
+//        fg.setVisible(true);  
+//    }  
+}  
--- src/test/Test.java
+++ src/test/Test.java
@@ -1,62 +1,45 @@
 package test;
+import java.io.IOException;
+import java.util.Scanner;
 
-import java.awt.FlowLayout;  
-import java.awt.event.ActionEvent;  
-import java.awt.event.ActionListener;  
-import javax.swing.JButton;  
-import javax.swing.JFrame;  
-import javax.swing.JProgressBar;  
-import javax.swing.JTextField;  
-public class Test extends JFrame {  
-    private static final long serialVersionUID = 1L;  
-    private static final String STR = "Completed : ";  
-    private JProgressBar progressBar = new JProgressBar();  
-    private JTextField text = new JTextField(10);  
-    private JButton start = new JButton("Start");  
-    private JButton end = new JButton("End");  
-    private boolean flag = false;  
-    private int count = 0;  
-    public Test() {  
-        this.setLayout(new FlowLayout());  
-        add(progressBar);  
-        text.setEditable(false);  
-        add(text);  
-        add(start);  
-        add(end);  
-        start.addActionListener(new Start());  
-        end.addActionListener(new End());  
-    }  
-          
-    private void go() {  
-        while (count < 100) {  
-            try {  
-                Thread.sleep(100);//这里比作要完成的某个耗时的工作  
-            } catch (InterruptedException e) {  
-                e.printStackTrace();  
-            }  
-                         //更新进度条和输入框  
-            if (flag) {  
-                count++;  
-                progressBar.setValue(count);  
-                text.setText(STR + String.valueOf(count) + "%");  
-            }  
-        }  
-    }  
-    private class Start implements ActionListener {  
-        public void actionPerformed(ActionEvent e) {  
-            flag = true;//设置开始更新的标志  
-            go();//开始工作  
-        }  
-    }  
-    private class End implements ActionListener {  
-        public void actionPerformed(ActionEvent e) {  
-            flag = false;//停止  
-        }  
-    }  
-//    public static void main(String[] args) {  
-//        Test fg = new Test();  
-//        fg.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
-//        fg.setSize(300, 100);  
-//        fg.setVisible(true);  
-//    }  
-}  
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class Test {
+	public Test(){
+		Document document = getDoc();
+		System.out.println(":");
+		Scanner input = new Scanner(System.in);
+		String label = input.next();
+		Elements element = getElement(document,label);
+		System.out.println(getText(element));
+	}
+	public Document getDoc(){
+		Document document;
+		try {
+			document = Jsoup.connect("http://www.baidu.com").get();
+			return document;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	public Elements getElement(Document doc, String label){
+		Elements element = doc.select(label);
+		return element;
+	}
+	
+	public String getText(Elements element){
+		return element.text();
+	}
+	
+	public static void main(String[] args){
+		Test ts = new Test();
+    }
+}
+
+
